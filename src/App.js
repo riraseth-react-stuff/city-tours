1b855268d84747f07ef3c1d7abefdb5869521f62
Łukasz Zabuski <lukasz@biuro.bcweb.pl>
2020-02-20 21:31:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "@fontawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
