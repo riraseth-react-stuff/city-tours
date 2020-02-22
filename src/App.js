1b72e1962f708607165f2a921c5415e3f69a5e44
Łukasz <riraseth@gmail.com>
2020-02-22 13:27:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.scss";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import Navbar from "./components/Navbar/Navbar";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import TourList from "./components/TourList";
 
 function App() {
